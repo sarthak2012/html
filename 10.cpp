2fb8aa5907313a776980db0dc239de4374ab4ebb
Sarthak Srivastava <Sarthaksrivastava5765@gmail.com>
2024-10-27 00:39:33 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -5,5 +5,98 @@
 <b> bold </bold>
 <i> italic </i>
 <u> underline </u>
-<hr>
-<>
+<hr>  horizontal ruler tag
+<sub>subscript</sub>
+<sup>superscript</sup>
+<header> 
+<main> 
+<footer>
+
+<section>
+Section Tag (For a section on your page)
+
+<article>
+ Article Tag For an article on your page  
+
+<aside>
+Aside Tag For content aside main content(ads) 
+
+
+Block List: Div Tags 
+   <address>
+   <article
+   <fieldset> 
+   <nav> 
+   <figcaption> 
+   <noscript>  
+   <aside> 
+   <figure> 
+   <ol>
+   <footer> 
+   <blockquote> 
+   <p> 
+   <form> 
+   <canvas> 
+   <pre>
+   <h1>-<h6> 
+   <header> 
+   <div> 
+   <hr> 
+   <dl> 
+   <dt> 
+   <li>
+   <main>
+Div Tag 
+Div is a container used for other HTML elements  
+Has Block Element (takes full width)
+
+Span Tag 
+Span is also a container used for other HTML elements 
+Inline Element (takes width as per size)
+
+List : Span Tags
+<dfn>
+<q>
+<em> 
+<samp> 
+<i> 
+<abbr> 
+<script> 
+<acronym> 
+<img> 
+<select> 
+<b> 
+<input> 
+<small> 
+<kbd> 
+<bdo> 
+<span> 
+<label> 
+<big> 
+<strong> 
+<br> 
+<map> 
+<sub> 
+<button> 
+<cite> 
+<object> 
+<sup>
+
+List in HTML 
+Lists are used to represent real life list data.
+
+unordered
+  <ul> 
+  <li> Apple </li> 
+  <li> Mango </li> 
+  </ul>
+ordered
+  <ol> <li> Apple </li>
+  <li> Mango </li> 
+  </ol>
+
+  Tables in HTML 
+Tables are used to represent real life table data. 
+<tr> used to display table row 
+<td> used to display table data 
+<th> used to display table header
